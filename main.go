2d4579402c5059b9fca25b7bc0bb2f5bafb6d3e3
Grigory <grengelmen@gmail.com>
2026-03-19 12:24:05 +0300

    Второе дз сделано

--- main.go
+++ main.go
@@ -9,3 +9,16 @@ func main() {
 	fmt.Println(EUR_RUB)
 
 }
+func getUserInput() {
+	var calculateFrom string
+	var calculateIn string
+	var calculateHowMuch int
+	fmt.Println("Введите исходную валюту")
+	fmt.Scan(&calculateFrom)
+	fmt.Println("Введите в какую валюту вы желаете конвертировать")
+	fmt.Scan(&calculateIn)
+	fmt.Println("Введите количество конвертируемой валюты")
+	fmt.Scan(&calculateHowMuch)
+
+}
+func convertCurrency(firstCurrency string, secondCurrency string, Quanity int) int {}
